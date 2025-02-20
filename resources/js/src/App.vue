9f77c02154cc3dec85adfd8275c3db66fe999aa1
ViacheslavHantiuk <Vgantyuk2@gmail.com>
2025-02-21 01:28:27 +0200

    Initial commit

---
+++ resources/js/src/App.vue
@@ -0,0 +1,18 @@
+<template >
+	<div>
+		test
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+	import PrimaryHeader from './components/PrimaryHeader.vue';
+
+	export default defineComponent({
+		components: {		},
+		setup() {
+			return {};
+		},
+	});
+</script>
+
---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Form for Zoho CRM Deals and Accounts using Vue.js and Laravel</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app"></div>
+    
+</body>
+</html>
